0da1224e420930fcb54573ecdc06ed75b20c943d
dmolinae <dmolinae@outlook.com>
2016-04-08 23:17:32 -0300

    Changed: - Dark apareance - Total register - Size of buttons

--- app/src/main/java/com/example/danie/rpsutil/MainActivity.java
+++ app/src/main/java/com/example/danie/rpsutil/MainActivity.java
@@ -5,15 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView_horas;
     private TextView textView_precio;
+    private TextView textView_total;
+
     private Button button1;
     private Button button2;
     private Button button3;
@@ -30,11 +36,23 @@ public class MainActivity extends AppCompatActivity {
     private Button button_sd;
     private Button button_50;
     private Button button_75;
+    private Button button_guardar;
 
     private String primera_hora;
     private String segunda_hora;
-    private Integer hrs1,hrs2,mins1,mins2,rest_hrs,rest_mins,precio;
+    private Integer hrs1,hrs2,mins1,mins2,rest_hrs,rest_mins,precio,precio_dsc;
+    private Integer total = 0;
     private Float rest;
+
+    public String zerosAtLeft(int number,int size){
+        String str_number = Integer.toString(number);
+        for(int i=0;i<size;i++){
+            if(str_number.length() < size){
+                str_number = "0"+str_number;
+            }
+        }
+        return str_number;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
         textView_horas = (TextView)findViewById(R.id.textView_horas);
         textView_precio = (TextView)findViewById(R.id.textView_precio);
+        textView_total = (TextView)findViewById(R.id.textView_total);
+
         button0 = (Button)findViewById(R.id.button0);
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
@@ -53,12 +73,15 @@ public class MainActivity extends AppCompatActivity {
         button7 = (Button)findViewById(R.id.button7);
         button8 = (Button)findViewById(R.id.button8);
         button9 = (Button)findViewById(R.id.button9);
+
         button_ac = (Button)findViewById(R.id.button_ac);
         button_del = (Button)findViewById(R.id.button_del);
 
         button_sd = (Button)findViewById(R.id.button_sd);
         button_50 = (Button)findViewById(R.id.button_50);
         button_75 = (Button)findViewById(R.id.button_75);
+
+        button_guardar = (Button)findViewById(R.id.button_guardar);
 
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -134,50 +157,64 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 textView_horas.setText("");
+                precio = 0;
+                precio_dsc = 0;
             }
         });
 
         button_del.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(textView_horas.getText().toString().substring(textView_horas.length()-1,textView_horas.length()).equals(":")){
-                    textView_horas.setText(textView_horas.getText().toString().substring(0,textView_horas.getText().length()-2));
+                if(textView_horas.getText().length() == 13){
+                    textView_horas.setText(textView_horas.getText().toString().substring(0,textView_horas.getText().length() - 5));
                 }
-                else if(textView_horas.getText().toString().substring(textView_horas.length()-1,textView_horas.length()).equals(" ")){
-                    textView_horas.setText(textView_horas.getText().toString().substring(0,textView_horas.getText().length()-4));
-                }
-                else {
-                    textView_horas.setText(textView_horas.getText().toString().substring(0, textView_horas.getText().length() - 1));
+                if(textView_horas.getText().length() != 0) {
+                    if (textView_horas.getText().toString().substring(textView_horas.length() - 1, textView_horas.length()).equals(":")) {
+                        textView_horas.setText(textView_horas.getText().toString().substring(0, textView_horas.getText().length() - 2));
+                    } else if (textView_horas.getText().toString().substring(textView_horas.length() - 1, textView_horas.length()).equals(" ")) {
+                        textView_horas.setText(textView_horas.getText().toString().substring(0, textView_horas.getText().length() - 4));
+                    } else {
+                        textView_horas.setText(textView_horas.getText().toString().substring(0, textView_horas.getText().length() - 1));
+                    }
                 }
             }
         });
 
         button_sd.setOnClickListener(new View.OnClickListener() {
-            Integer result;
             @Override
             public void onClick(View v) {
-                result = precio;
-                textView_precio.setText("$ "+result.toString());
+                precio_dsc = precio;
+                textView_precio.setText("$ "+precio_dsc.toString());
             }
         });
 
         button_50.setOnClickListener(new View.OnClickListener() {
-            Integer result;
             @Override
             public void onClick(View v) {
-                result = (precio/2);
-                textView_precio.setText("$ "+result.toString());
+                precio_dsc = (precio/2);
+                textView_precio.setText("$ "+precio_dsc.toString());
             }
         });
 
         button_75.setOnClickListener(new View.OnClickListener() {
-            Integer result;
             @Override
             public void onClick(View v) {
-                result = (precio/4);
-                textView_precio.setText("$ "+result.toString());
+                precio_dsc = (precio/4);
+                textView_precio.setText("$ "+precio_dsc.toString());
             }
         });
+
+        button_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = total + precio_dsc;
+                textView_total.setText("$ "+total.toString());
+                textView_horas.setText("");
+                precio = 0;
+                precio_dsc = 0;
+            }
+        });
+
 
 
         textView_horas.addTextChangedListener(new TextWatcher() {
@@ -188,16 +225,21 @@ public class MainActivity extends AppCompatActivity {
             }
 
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() != 13) {
+                textView_horas.setGravity(Gravity.CENTER);
+                if (s.length() < 13) {
                     textView_precio.setText("");
                     if (s.length() == 2) {
                         textView_horas.setText(textView_horas.getText().toString() + ":");
                     }
                     if (s.length() == 5) {
                         textView_horas.setText(textView_horas.getText().toString() + " - ");
+                        Calendar horaActual = Calendar.getInstance();
+                        textView_horas.setText(textView_horas.getText().toString() + zerosAtLeft(horaActual.get(Calendar.HOUR_OF_DAY), 2));
                     }
                     if (s.length() == 10) {
                         textView_horas.setText(textView_horas.getText().toString() + ":");
+                        Calendar horaActual = Calendar.getInstance();
+                        textView_horas.setText(textView_horas.getText().toString() + zerosAtLeft(horaActual.get(Calendar.MINUTE), 2));
                     }
                 } else {
                     primera_hora = s.toString().substring(0, 2) + s.toString().substring(3, 5);
@@ -238,6 +280,7 @@ public class MainActivity extends AppCompatActivity {
                     if (rest < 0.17) {
                         precio = 0;
                     }
+                    precio_dsc = precio;
                     textView_precio.setText("$ " + precio.toString());
                 }
             }
